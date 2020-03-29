59c8e9198299b6aff148911b269c6e9ba565607e
Ygor Mattos <ygormattos.b@gmail.com>
2020-03-29 13:54:21 -0300

    Pages CSS done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './App.css';
+
+import './global.css'
+
+import Routes from './routes'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Routes />
   );
 }
 
